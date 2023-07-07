5b3024c7826466b9194a283e2ae1b8ebadf3af1c
yong <yongho6800@gmail.com>
2023-07-07 20:20:42 +0900

    feat/friend-serach-page

--- matrip/src/App.tsx
+++ matrip/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import FriendSearch from "./pages/FriendSearch"
+
 
 function App() {
    return (
@@ -9,6 +11,10 @@ function App() {
             <Route
                path="/"
                element={<Main />}
+            />
+            <Route
+               path="/friendSearch"
+               element={<FriendSearch />}
             />
          </Routes>
       </BrowserRouter>
