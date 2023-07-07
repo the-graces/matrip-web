dd3aa8790d8b1605b8e89f274678bf10796938eb
yong <yongho6800@gmail.com>
2023-07-07 18:59:30 +0900

    refactor/styled component separation

--- matrip/src/App.tsx
+++ matrip/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import ItineraryRegister from "./pages/ItineraryRegister";
+import ItineraryRegister from "./pages/ItineraryInfo";
 
 function App() {
    return (
