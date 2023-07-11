ec3f121c75dee73201766108476151bb82fcb6b3
최용호 <yongho6800@gmail.com>
2023-07-11 15:06:49 +0900

    feat: 지도 컨트롤
    * feat: 우클릭 위치정보 받아오기
    
    * feat: 지도 우클릭 위치정보 받기
    
    * feat: 위치 검색 input 기능 개선 및 maker icon추가

--- matrip/src/pages/MapSearch/mapSearchComponents/GoogleMap.tsx
+++ matrip/src/pages/MapSearch/mapSearchComponents/GoogleMap.tsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {
   GoogleMap,
   LoadScript,
   Marker,
   StandaloneSearchBox
 } from '@react-google-maps/api';
-import { styled } from 'styled-components';
+
+import { performReverseGeocode } from './geocode'
 
 import * as ms from '../mapPageStyle';
+
 
 interface GoogleMapComponentProps {
   zoom: number;
   center: { lat: number; lng: number };
   // maker는 여러개가 될 수 있음
-  markers: Array<{ lat: number; lng: number }>;
+  // markers: { lat: number; lng: number };
 }
 
-interface PickLocationInfo {
-  name: string;
-  lat: number;
-  lng: number;
-}
 
 const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({
   zoom,
   center,
-  markers
+  // markers
 }) => {
   const [mapCenter, setMapCenter] = useState(center);
   const [searchBox, setSearchBox] = useState<google.maps.places.SearchBox>();
-  const [pickedLoction, setPickedLocation] = useState<PickLocationInfo>();
 
-  console.log(mapCenter);
-  console.log(pickedLoction);
+  //우클릭한 위치에 대한 정보
+  const [clickedLocation, setClickedLocation] = useState<google.maps.LatLngLiteral>();
+  const [clickedAddress, setClickedAddress] = useState<string>('');
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  // 지도 우클릭하면 바뀌는 위치 정보를 검색창에도 반영 
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = clickedAddress;
+    }
+  }, [clickedAddress]);
+
+
 
   //StandaloneSearchBox 컴포넌트의 인스턴스를 참조
   const onLoad = (ref: google.maps.places.SearchBox) => {
@@ -46,7 +54,8 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({
   const mapOptions = {
     mapTypeControl: false,
     fullscreenControl: false,
-    streetViewControl: false
+    streetViewControl: false,
+    styles: ms.customStyles
   };
 
   /**
@@ -60,19 +69,40 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({
       if (places && places.length === 1) {
         // 위도 경도 값
         const place = places[0].geometry?.location;
-        const placeName = places[0].name;
+        // const placeName = places[0].name;
+        const placeAddress = places[0].formatted_address;
+        console.log(places[0])
 
-        if (place && placeName) {
+        if (place && placeAddress) {
           setMapCenter({ lat: place.lat(), lng: place.lng() });
-          setPickedLocation({
-            name: placeName,
-            lat: place.lat(),
-            lng: place.lng()
-          });
+          setClickedLocation({ lat: place.lat(), lng: place.lng() });
+          setClickedAddress(placeAddress)
         }
       }
     }
   };
+
+  const onMapRightClick = (e: google.maps.MapMouseEvent) => {
+    const location = e.latLng;
+
+    if (!location){return} 
+
+    const newLoc = {
+      lat: e.latLng?.lat()!,
+      lng: e.latLng?.lng()!,
+    }
+
+    // Reverse geocode를 실행하여 주소 가져오기
+    if (location) {
+      performReverseGeocode(location, (res) => {
+        if (res && newLoc) {
+          setClickedAddress(res.address)
+          setClickedLocation(newLoc);
+        }
+      });
+    }
+  };
+
 
   return (
     <>
@@ -81,7 +111,7 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({
         libraries={['places']}
       >
         <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
-          <ms.LocationSearchInput type='text' placeholder='Search location' />
+          <ms.LocationSearchInput ref={inputRef} type='text' placeholder='Search location' />
         </StandaloneSearchBox>
         <GoogleMap
           mapContainerStyle={{
@@ -93,10 +123,24 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({
           zoom={zoom}
           center={mapCenter}
           options={mapOptions}
+          onRightClick={onMapRightClick}
         >
-          {mapCenter && <Marker key={'selectedPlace'} position={mapCenter} />}
+          {clickedLocation &&
+            <Marker
+              key={clickedLocation.lat + clickedLocation.lng}
+              position={{ lat: clickedLocation.lat, lng: clickedLocation.lng }}
+              onClick={(e: google.maps.MapMouseEvent) => console.log('cliecked')}
+              icon={ms.markerIcon}
+            />
+          }
         </GoogleMap>
       </LoadScript>
+      {clickedAddress &&
+        <div>
+          <h3>FOR DEV : 지도위에 우클릭하시면 됩니당</h3>
+          <h1>{clickedAddress}</h1>
+        </div>
+      }
     </>
   );
 };
